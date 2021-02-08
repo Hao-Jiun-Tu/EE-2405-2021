cd24c3fde6d0eb802c920d5af632c144858e0eee
Hao-Jiun-Tu <61753385+Hao-Jiun-Tu@users.noreply.github.com>
2021-02-08 01:52:20 -0800

    Add files via upload

--- mbed09/9_1_RPC_Serial/BUILD/B_L4S5I_IOT01A/GCC_ARM/main.d
+++ mbed09/9_1_RPC_Serial/BUILD/B_L4S5I_IOT01A/GCC_ARM/main.d
@@ -322,6 +322,8 @@ BUILD/B_L4S5I_IOT01A/GCC_ARM/main.o: main.cpp \
  /home/ee2405/ee2405/mbed-os-build/hal/include/hal/mpu_api.h \
  /home/ee2405/ee2405/mbed-os-build/platform/include/platform/ScopedRamExecutionLock.h \
  /home/ee2405/ee2405/mbed-os-build/platform/include/platform/mbed_stats.h \
- mbed_rpc/mbed_rpc.h mbed_rpc/rpc.h mbed_rpc/Arguments.h \
+ mbed_rpc/mbed_rpc/mbed_rpc.h mbed_rpc/mbed_rpc/rpc.h \
+ mbed_rpc/mbed_rpc/Arguments.h \
  /home/ee2405/ee2405/mbed-os-build/connectivity/mbedtls/include/mbedtls/platform.h \
- mbed_rpc/RPCVariable.h mbed_rpc/RPCFunction.h mbed_rpc/RpcClasses.h
+ mbed_rpc/mbed_rpc/RPCVariable.h mbed_rpc/mbed_rpc/RPCFunction.h \
+ mbed_rpc/mbed_rpc/RpcClasses.h
--- mbed09/9_1_RPC_Serial/main.cpp
+++ mbed09/9_1_RPC_Serial/main.cpp
@@ -2,9 +2,9 @@
 #include "mbed_rpc.h"
 
 /**
- *  This example program has been updated to use the RPC implementation in the new mbed libraries.
- *  This example demonstrates using RPC over serial
- */
+*  This example program has been updated to use the RPC implementation in the new mbed libraries.
+*  This example demonstrates using RPC over serial
+*/
 
 //Use the RPC enabled wrapped class  - see RpcClasses.h for more info
 RpcDigitalOut myled(LED1,"myled");
@@ -13,26 +13,25 @@ RpcDigitalOut myled3(LED3,"myled3");
 BufferedSerial pc(USBTX, USBRX);
 
 int main() {
-    //The mbed RPC classes are now wrapped to create an RPC enabled version - see RpcClasses.h so don't add to base class
-    // receive commands, and send back the responses
-    char buf[256], outbuf[256];
-    FILE *devin = fdopen(&pc, "r");
-    FILE *devout = fdopen(&pc, "w");
+   //The mbed RPC classes are now wrapped to create an RPC enabled version - see RpcClasses.h so don't add to base class
+   // receive commands, and send back the responses
+   char buf[256], outbuf[256];
+   FILE *devin = fdopen(&pc, "r");
+   FILE *devout = fdopen(&pc, "w");
 
-    while(1) {
-        memset(buf, 0, 256);      // clear buffer
+   while(1) {
+      memset(buf, 0, 256);      // clear buffer
 
-        for(int i=0; ; i++) {
+      for(int i=0; ; i++) {
             char recv = fgetc(devin);
             if ( recv == '\n' ) {
-                printf("\r\n");
-                break;
+               printf("\r\n");
+               break;
             }
             buf[i] = fputc(recv, devout);
-        }
-        //Call the static call method on the RPC class
-        RPC::call(buf, outbuf);
-        printf("%s\r\n", outbuf);
-    }
+      }
+      //Call the static call method on the RPC class
+      RPC::call(buf, outbuf);
+      printf("%s\r\n", outbuf);
+   }
 }
-
